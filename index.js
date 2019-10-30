fdd72f29d35a5a7a0cf7ae0be66335aaa60ad86f
vanna sok <vannasok@vannas-MBP.fios-router.home>
2019-10-30 12:57:14 -0700

    install helmet, test the server

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+const server = require('./server.js');
+const port = 4000;
+server.listen(port, () => {
+   console.log(`* Server Running on http://localhost:${port} *`);
+});
--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = 'express';
-
+const express = require('express');
+const helmet = require('helmet');
 const server = express();
 
+//custom middleware
+server.use(helmet());
+server.use(logger);
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
-
 function logger(req, res, next) {
-
-};
+   const time = new Date().toLocaleTimeString();
+   const date = new Date().toLocaleDateString();
+   console.log(
+      `${req.method} Request | http://localhost:4000${req.url} | ${date} , ${time}`
+   );
+   next();
+}
 
 module.exports = server;
