0f0f577b58cd2070dd1250314cdd8f8e137da695
vanna sok <vannasok@vannas-MBP.fios-router.home>
2019-10-31 10:40:52 -0700

    env port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ server.use(logger);
 server.use('/users', userRouter);
 
 server.get('/', (req, res) => {
-   res.send(`<h2>Let's write some middleware!</h2>`);
+   res.json({ message: 'Lets write some middleware' });
 });
 
 function logger(req, res, next) {
